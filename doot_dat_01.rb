e0d52f009e549204907b2ab6f86e3b264fa916fc
Daniel Rosenberg <danielrosenberg@DanieldeMacBook-Pro.local>
2019-03-29 15:43:15 +0800

    my first doot dad

---
+++ doot_dat_01.rb
@@ -0,0 +1,30 @@
+
+
+live_loop:play do
+  play 62.4, amp: 0.5
+  use_synth :dtri
+  play 25, amp: 5
+  sleep 0.5
+  play 65.7, amp: 0.5
+  play 25, amp: 5
+  sleep 0.5
+  play 69, amp: 0.5
+  play 25, amp: 5
+  sleep 0.5
+  play :A, amp: 0.5
+  play 25, amp: 5
+  sleep 0.5
+  play :C, amp: 0.5
+  play 25, amp: 5
+  sleep 0.5
+  play :E, amp: 0.5
+  play 25, amp: 5
+  sleep 0.5
+  play :A, amp: 0.5
+  play 25, amp: 5
+  sleep 1
+end
+
+
+
+
